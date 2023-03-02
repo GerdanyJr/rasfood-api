2ff1c5f1e1d5aaf6e1b2fbd21fb6039f950bdb9b
Gerdany Junior <Asa_lindo@hotmail.com>
2023-03-02 12:55:57 -0300

    Implementação da ordenação em EndereçoController

--- src/main/java/com/rasmoo/api/rasfood/rasfoodapi/controller/EndereçoController.java
+++ src/main/java/com/rasmoo/api/rasfood/rasfoodapi/controller/EndereçoController.java
@@ -4,8 +4,8 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +20,7 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rasmoo.api.rasfood.rasfoodapi.entity.Endereço;
 import com.rasmoo.api.rasfood.rasfoodapi.repository.EndereçoRepository;
+import com.rasmoo.api.rasfood.rasfoodapi.util.SortVerification;
 
 @RequestMapping(value = "/endereco")
 @RestController
@@ -30,32 +31,38 @@ public class EndereçoController {
 
     @Autowired
     private ObjectMapper objectMapper;
-    
+
     @GetMapping
-    public ResponseEntity<Page<Endereço>> consultarTodos(@RequestParam("actualPage")Integer actualPage){
-        Pageable pageable = PageRequest.of(actualPage, 5);
+    public ResponseEntity<Page<Endereço>> consultarTodos(@RequestParam("actualPage") Integer actualPage,
+            @RequestParam(value = "sort", required = false) Sort.Direction sort,
+            @RequestParam(value = "property", required = false) String property) {
+        Pageable pageable = SortVerification.verifySort(actualPage, sort, property);
         return ResponseEntity.status(HttpStatus.OK).body(endereçoRepository.findAll(pageable));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Endereço> consultarPorId(@PathVariable("id") Integer id){
+    public ResponseEntity<Endereço> consultarPorId(@PathVariable("id") Integer id) {
         Optional<Endereço> endereçoEncontrado = endereçoRepository.findById(id);
-        if(!endereçoEncontrado.isPresent()){
+        if (!endereçoEncontrado.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
         return ResponseEntity.status(HttpStatus.OK).body(endereçoEncontrado.get());
     }
 
     @GetMapping("/cep/{cep}")
-    public ResponseEntity<Page<Endereço>> consultarPorCep(@PathVariable("cep")String cep,@RequestParam("actualPage")Integer actualPage){
-        Pageable pageable = PageRequest.of(actualPage, 5);
-        return ResponseEntity.status(HttpStatus.OK).body(endereçoRepository.findByCep(cep,pageable));
+    public ResponseEntity<Page<Endereço>> consultarPorCep(@PathVariable("cep") String cep,
+            @RequestParam("actualPage") Integer actualPage,
+            @RequestParam(value = "sort", required = false) Sort.Direction sort,
+            @RequestParam(value = "property", required = false) String property) {
+        Pageable pageable = SortVerification.verifySort(actualPage, sort, property);
+        return ResponseEntity.status(HttpStatus.OK).body(endereçoRepository.findByCep(cep, pageable));
     }
 
     @PatchMapping("/1/{id}")
-    public ResponseEntity<Endereço> atualizarEndereço(@PathVariable("id") Integer id,@RequestBody Endereço endereço) throws JsonMappingException{
+    public ResponseEntity<Endereço> atualizarEndereço(@PathVariable("id") Integer id, @RequestBody Endereço endereço)
+            throws JsonMappingException {
         Optional<Endereço> endereçoEncontrado = endereçoRepository.findById(id);
-        if(!endereçoEncontrado.isPresent()){
+        if (!endereçoEncontrado.isPresent()) {
             return ResponseEntity.status(HttpStatus.OK).body(null);
         }
         objectMapper.updateValue(endereçoEncontrado.get(), endereço);
