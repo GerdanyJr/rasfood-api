866f9b6fef96302a048fcdf6575b7695d31ded63
Gerdany Junior <Asa_lindo@hotmail.com>
2023-03-02 11:16:31 -0300

    Implementada a paginação na Classe Cardapio

--- src/main/java/com/rasmoo/api/rasfood/rasfoodapi/controller/CardapioController.java
+++ src/main/java/com/rasmoo/api/rasfood/rasfoodapi/controller/CardapioController.java
@@ -1,9 +1,11 @@
 package com.rasmoo.api.rasfood.rasfoodapi.controller;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,11 +15,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.rasmoo.api.rasfood.rasfoodapi.dto.CardapioDto;
 import com.rasmoo.api.rasfood.rasfoodapi.entity.Cardapio;
 import com.rasmoo.api.rasfood.rasfoodapi.repository.CardapioProjection;
 import com.rasmoo.api.rasfood.rasfoodapi.repository.CardapioRepository;
@@ -33,8 +35,9 @@ public class CardapioController {
     private ObjectMapper objectMapper;
 
     @GetMapping
-    public ResponseEntity<List<Cardapio>> consultarTodos() {
-        return ResponseEntity.status(HttpStatus.OK).body(this.cardapioRepository.findAll());
+    public ResponseEntity<Page<Cardapio>> consultarTodos(@RequestParam("actualPage")Integer actualPage) {
+        Pageable pageable = PageRequest.of(actualPage, 3);
+        return ResponseEntity.status(HttpStatus.OK).body(this.cardapioRepository.findAll(pageable));
     }
 
     @GetMapping("/consultar/{id}")
@@ -47,17 +50,19 @@ public class CardapioController {
     }
 
     @GetMapping("/categoria/{categoriaid}")
-    public ResponseEntity<List<Cardapio>> encontrarPorCategoria(@PathVariable("categoriaid")Integer categoriaid){
-        List<Cardapio> cardapiosEncontrados = this.cardapioRepository.consultarPorCategoria(categoriaid);
+    public ResponseEntity<Page<Cardapio>> encontrarPorCategoria(@PathVariable("categoriaid")Integer categoriaid,@RequestParam("actualPage")Integer actualPage){
+        Pageable pageable = PageRequest.of(actualPage, 5);
+        Page<Cardapio> cardapiosEncontrados = this.cardapioRepository.consultarPorCategoria(categoriaid,pageable);
         if (cardapiosEncontrados.isEmpty()){
             return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
         }
         return ResponseEntity.status(HttpStatus.OK).body(cardapiosEncontrados);
     }
 
-    @GetMapping("/cardapio/{id}/disponivel")
-    public ResponseEntity<List<CardapioProjection>> encontrarTodos(@PathVariable("id")Integer id){
-        List<CardapioProjection> cardapiosEncontrados = this.cardapioRepository.consultarPorCategoriaProjection(id);
+    @GetMapping("/{id}/disponivel")
+    public ResponseEntity<Page<CardapioProjection>> encontrarTodos(@PathVariable("id")Integer id,@RequestParam("actualPage")Integer actualPage){
+        Pageable pageable = PageRequest.of(actualPage, 5);
+        Page<CardapioProjection> cardapiosEncontrados = this.cardapioRepository.consultarPorCategoriaProjection(id,pageable);
         if (cardapiosEncontrados.isEmpty()){
             return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
         }
--- src/main/java/com/rasmoo/api/rasfood/rasfoodapi/repository/CardapioRepository.java
+++ src/main/java/com/rasmoo/api/rasfood/rasfoodapi/repository/CardapioRepository.java
@@ -1,21 +1,27 @@
 package com.rasmoo.api.rasfood.rasfoodapi.repository;
 
-import java.util.List;
+import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.rasmoo.api.rasfood.rasfoodapi.dto.CardapioDto;
 import com.rasmoo.api.rasfood.rasfoodapi.entity.Cardapio;
 
-public interface CardapioRepository extends JpaRepository<Cardapio,Integer>{
+public interface CardapioRepository extends PagingAndSortingRepository<Cardapio,Integer>{
     
     @Query("SELECT new com.rasmoo.api.rasfood.rasfoodapi.dto.CardapioDto(c.nome,c.descricao,c.valor,c.categoria.nome) FROM Cardapio c WHERE c.nome LIKE %:nome% AND c.disponivel = true")
-    List<CardapioDto> findAllByNome(String nome);
+    Page<CardapioDto> findAllByNome(String nome, Pageable pageable);
 
-    @Query(value = "SELECT * FROM cardapio c WHERE c.categoria_id = ?1 AND c.disponivel = true",nativeQuery = true)
-    List<Cardapio> consultarPorCategoria(Integer categoria);
+    @Query(value = "SELECT * FROM cardapio c WHERE c.categoria_id = ?1 AND c.disponivel = true",nativeQuery = true,countQuery = "SELECT count(*) FROM cardapio")
+    Page<Cardapio> consultarPorCategoria(Integer categoria, Pageable pageable);
 
     @Query(value = "SELECT c.nome AS nome,c.descricao AS descricao,c.valor AS valor,cat.nome AS nomeCategoria FROM cardapio c INNER JOIN categorias cat ON c.categoria_id = cat.id WHERE c.categoria_id = :id AND c.disponivel = true",nativeQuery = true)
-    List<CardapioProjection> consultarPorCategoriaProjection(Integer id);
+    Page<CardapioProjection> consultarPorCategoriaProjection(Integer id, Pageable pageable);
+
+    Optional<Cardapio> findById(Integer id);
+
+    Cardapio save(Cardapio cardapio);
 }
