2f7c2a3acbfd04f8c5ed32326d2492f692ec6975
Gerdany Junior <Asa_lindo@hotmail.com>
2023-03-02 11:38:13 -0300

    Implementada paginação em categoria

--- src/main/java/com/rasmoo/api/rasfood/rasfoodapi/controller/CategoriaController.java
+++ src/main/java/com/rasmoo/api/rasfood/rasfoodapi/controller/CategoriaController.java
@@ -1,9 +1,11 @@
 package com.rasmoo.api.rasfood.rasfoodapi.controller;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -31,13 +34,15 @@ public class CategoriaController {
     private ObjectMapper objectMapper;
 
     @GetMapping
-    public ResponseEntity<List<Categoria>> encontrarTodos(){
-        return ResponseEntity.status(HttpStatus.OK).body(categoriaRepository.findAll());
+    public ResponseEntity<Page<Categoria>> encontrarTodos(@RequestParam("actualPage")Integer actualPage){
+        Pageable pageable = PageRequest.of(actualPage, 5);
+        return ResponseEntity.status(HttpStatus.OK).body(categoriaRepository.findAll(pageable));
     }
 
     @GetMapping("/consultar/{nome}")
-    public ResponseEntity<List<Categoria>> encontrarPorNome(@PathVariable("nome")String nome){
-        List<Categoria> categoriasEncontradas = categoriaRepository.findByNomeStartingWith(nome);
+    public ResponseEntity<Page<Categoria>> encontrarPorNome(@PathVariable("nome")String nome,@RequestParam("actualPage")Integer actualPage){
+        Pageable pageable = PageRequest.of(actualPage, 5);
+        Page<Categoria> categoriasEncontradas = categoriaRepository.findByNomeStartingWith(nome,pageable);
         if(categoriasEncontradas.isEmpty()){
             return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
         }
--- src/main/java/com/rasmoo/api/rasfood/rasfoodapi/repository/CategoriaRepository.java
+++ src/main/java/com/rasmoo/api/rasfood/rasfoodapi/repository/CategoriaRepository.java
@@ -1,12 +1,21 @@
 package com.rasmoo.api.rasfood.rasfoodapi.repository;
 
-import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.rasmoo.api.rasfood.rasfoodapi.entity.Categoria;
 
-public interface CategoriaRepository extends JpaRepository<Categoria,Integer> {
+public interface CategoriaRepository extends PagingAndSortingRepository<Categoria,Integer> {
     
-    List<Categoria> findByNomeStartingWith(String nome);
+    Page<Categoria> findByNomeStartingWith(String nome,Pageable pageable);
+
+    Categoria save(Categoria categoria);
+
+    Optional<Categoria> findById(Integer id);
+
+    Categoria delete(Categoria categoria);
 }
