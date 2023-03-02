a85a3ded5004004fca0062ea42c1eace3b87b052
Gerdany Junior <Asa_lindo@hotmail.com>
2023-03-02 13:06:29 -0300

    Implementada ordenação em Cardapio

--- src/main/java/com/rasmoo/api/rasfood/rasfoodapi/controller/CardapioController.java
+++ src/main/java/com/rasmoo/api/rasfood/rasfoodapi/controller/CardapioController.java
@@ -4,8 +4,8 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -23,6 +23,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rasmoo.api.rasfood.rasfoodapi.entity.Cardapio;
 import com.rasmoo.api.rasfood.rasfoodapi.repository.CardapioProjection;
 import com.rasmoo.api.rasfood.rasfoodapi.repository.CardapioRepository;
+import com.rasmoo.api.rasfood.rasfoodapi.util.SortVerification;
 
 @RequestMapping(value = "/cardapio")
 @RestController
@@ -35,42 +36,50 @@ public class CardapioController {
     private ObjectMapper objectMapper;
 
     @GetMapping
-    public ResponseEntity<Page<Cardapio>> consultarTodos(@RequestParam("actualPage")Integer actualPage) {
-        Pageable pageable = PageRequest.of(actualPage, 3);
+    public ResponseEntity<Page<Cardapio>> consultarTodos(@RequestParam("actualPage") Integer actualPage,
+            @RequestParam(value = "sort",required = false) Sort.Direction sort, @RequestParam(value = "property",required = false) String property) {
+        Pageable pageable = SortVerification.verifySort(actualPage, sort, property);
         return ResponseEntity.status(HttpStatus.OK).body(this.cardapioRepository.findAll(pageable));
     }
 
     @GetMapping("/consultar/{id}")
-    public ResponseEntity<Cardapio> consultarPorId(@PathVariable Integer id){
+    public ResponseEntity<Cardapio> consultarPorId(@PathVariable Integer id) {
         Optional<Cardapio> cardapioEncontrado = this.cardapioRepository.findById(id);
-        if (!cardapioEncontrado.isPresent()){
+        if (!cardapioEncontrado.isPresent()) {
             return ResponseEntity.status(HttpStatus.OK).body(cardapioEncontrado.get());
         }
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
     }
 
     @GetMapping("/categoria/{categoriaid}")
-    public ResponseEntity<Page<Cardapio>> encontrarPorCategoria(@PathVariable("categoriaid")Integer categoriaid,@RequestParam("actualPage")Integer actualPage){
-        Pageable pageable = PageRequest.of(actualPage, 5);
-        Page<Cardapio> cardapiosEncontrados = this.cardapioRepository.consultarPorCategoria(categoriaid,pageable);
-        if (cardapiosEncontrados.isEmpty()){
+    public ResponseEntity<Page<Cardapio>> encontrarPorCategoria(@PathVariable("categoriaid") Integer categoriaid,
+            @RequestParam("actualPage") Integer actualPage,
+            @RequestParam(value = "sort",required = false)Sort.Direction sort,
+            @RequestParam(value= "property",required = false)String property) {
+        Pageable pageable = SortVerification.verifySort(actualPage, sort, property);
+        Page<Cardapio> cardapiosEncontrados = this.cardapioRepository.consultarPorCategoria(categoriaid, pageable);
+        if (cardapiosEncontrados.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
         }
         return ResponseEntity.status(HttpStatus.OK).body(cardapiosEncontrados);
     }
 
     @GetMapping("/{id}/disponivel")
-    public ResponseEntity<Page<CardapioProjection>> encontrarTodos(@PathVariable("id")Integer id,@RequestParam("actualPage")Integer actualPage){
-        Pageable pageable = PageRequest.of(actualPage, 5);
-        Page<CardapioProjection> cardapiosEncontrados = this.cardapioRepository.consultarPorCategoriaProjection(id,pageable);
-        if (cardapiosEncontrados.isEmpty()){
+    public ResponseEntity<Page<CardapioProjection>> encontrarTodos(@PathVariable("id") Integer id,
+            @RequestParam("actualPage") Integer actualPage,
+            @RequestParam(value = "sort",required = false)Sort.Direction sort,
+            @RequestParam(value = "property",required = false)String property) {
+        Pageable pageable = SortVerification.verifySort(actualPage, sort, property);
+        Page<CardapioProjection> cardapiosEncontrados = this.cardapioRepository.consultarPorCategoriaProjection(id,
+                pageable);
+        if (cardapiosEncontrados.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
         }
         return ResponseEntity.status(HttpStatus.OK).body(cardapiosEncontrados);
     }
 
     @PostMapping("/salvar")
-    public ResponseEntity<Cardapio> salvar(@RequestBody Cardapio cardapio){
+    public ResponseEntity<Cardapio> salvar(@RequestBody Cardapio cardapio) {
         return ResponseEntity.status(HttpStatus.OK).body(this.cardapioRepository.save(cardapio));
     }
 
@@ -86,9 +95,9 @@ public class CardapioController {
     }
 
     @DeleteMapping("/excluir/{id}")
-    public ResponseEntity<Cardapio> excluir(@PathVariable("id")Integer id){
+    public ResponseEntity<Cardapio> excluir(@PathVariable("id") Integer id) {
         Optional<Cardapio> cardapioEncontrado = this.cardapioRepository.findById(id);
-        if (!cardapioEncontrado.isPresent()){
+        if (!cardapioEncontrado.isPresent()) {
             return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
         }
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
